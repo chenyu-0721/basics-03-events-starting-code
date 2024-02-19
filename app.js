6a711f647022922dfe17f963d71efe670b5c9600
陳冠宇 <chen630602@gmail.com>
2024-02-19 13:11:50 +0800

    2024/02/19 Vue-basic-03
    1.v-on:click                     監聽按鈕
    2.v-once                          鎖定初始值
    3.v-on:input                    監聽輸入的值
    4.v-on:submit.prevent  防止修飾符

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
+      name: "",
+      confirmedName: "",
     };
+  },
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      event.preventDefault();
+      alert("Submitted!");
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    remove(num) {
+      this.counter = this.counter - num;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
