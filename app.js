f8bd73daf716e015b1ec25a2ed0b97c5e0a4529a
陳冠宇 <chen630602@gmail.com>
2024-02-20 14:58:27 +0800

    2024/02/20
    新增 watch ,  computed 用法

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 10,
+      counter: 0,
       name: "",
-      confirmedName: "",
+      // fullname: "",
+      lastname: "",
     };
+  },
+  watch: {
+    // 監聽 當更改時會重新執行
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000); // 超過50 2秒後歸零
+      }
+    },
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = value + " " + this.lastname;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = this.name + " " + value;
+    //   }
+    // },
+  },
+  computed: {
+    //計算
+    fullname() {
+      console.log("Running again...");
+      if (this.name === "" || this.lastname === "") {
+        return "";
+      }
+      return this.name + " " + this.lastname;
+    },
   },
   methods: {
     outputFullname() {
       if (this.name === "") {
         return "";
       }
-      return this.name + " " + "Schwarzmuller";
+      return this.name;
     },
     confirmInput() {
       this.confirmedName = this.name;
@@ -27,6 +63,7 @@ const app = Vue.createApp({
     },
     resetInput() {
       this.name = "";
+      this.lastname = "";
     },
   },
 });
